b99e45189d6a51619835c102ed90d7cb1fe50fb6
aamir2712 <aamirabbas2001@gmail.com>
2013-01-16 20:52:33 -0500

    dns query

---
+++ aamir2712/dns.js
@@ -0,0 +1,5 @@
+var dns =require('dns');
+dns.lookup('google.com', function(err,ip){
+if(err) return handleError(err);
+console.log('google.com resolved to %s', ip);
+});
---
+++ aamir2712/nodeServer.js
@@ -0,0 +1,9 @@
+ require('http').createServer(function handleRequest(req, res){
+res.writeHead(200, {'content-type': 'text/plain'});
+res.end('Hello World!');
+})
+.listen(8080);
+
+//server.on('request',handleRequest);
+
+
